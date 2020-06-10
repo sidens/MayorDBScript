4088bd623bd60b27451b256dddd076695d02d04d
sidens <sidens@gmail.com>
2020-06-09 22:15:52 -0400

    added file parameters, and removed need for selenium

--- script.py
+++ script.py
@@ -1,62 +1,37 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
+import requests
+import sys
 
 
-def search_selenium (searchcity, searchstate):
-	#takes city, state parameters
-	#returns pagesource
-	driver = webdriver.Chrome()
-	driver.implicitly_wait(10)
-	driver.get('https://www.usmayors.org/mayors/')
 
-	search_input = '//*[@id="searchform"]/input[1]'
-	search_submit = '//*[@id="searchform"]/input[2]'
+def search_post (searchcity, searchstate): #unable to get working due to incomplete response - likely due to invalid/incomplete headers?
+	url = 'https://www.usmayors.org/mayors/meet-the-mayors'
+	session = requests.session()
+	payload = 	{'searchTerm': searchcity,
+					'submit': 'search'}	
+	headers = {
+				'authority': 'www.usmayors.org',
+				'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+				'origin':'https://www.usmayors.org',
+				'referer':'https://www.usmayors.org/mayors/meet-the-mayors',
+				'upgrade-insecure-requests': '1',
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+			  }
 
-	driver.find_element_by_xpath(search_input).send_keys(searchcity)
-	driver.find_element_by_xpath(search_submit).click()
-
-	try:
-	    pagesource = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="post-330"]/div/ul')))
-	    print ("Page is ready!")
-	    return driver.page_source
-	except TimeoutException:
-	    print ("Loading took too much time!")
-	finally:
-		driver.close()
-
-#def search_mayors_post (searchcity, searchstate): #unable to get working due to incomplete response - likely due to invalid/incomplete headers?
-	# url = 'https://www.usmayors.org/mayors/'
-	# session = requests.session()
-	# payload = 	{'searchTerm': searchcity,
-	# 				'submit': 'search'}	
-	# headers = {
-	# 			# 'authority': 'www.usmayors.org',
-	# 			# 'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	# 			# 'origin':'https://www.usmayors.org',
-	# 			# 'referer':'https://www.usmayors.org/mayors/meet-the-mayors',
-	# 			# 'upgrade-insecure-requests': '1',
-	# 			# 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
-	# 		  }
-
-	# response = requests.post(url, data=payload, headers = headers)
-	# # print (response.text)
-	# return response.text
+	response = requests.post(url, data=payload, headers = headers)
+	#print (response.text)
+	return response.text
 
 
 def parse_mayors_data (searchcity, searchstate):
 
-	#Call selenium based lookup function and capture the HTML
-	rawhtml = search_selenium(searchcity, searchstate)
+	#Call direct post request and capture the HTML response
+	rawhtml = search_post(searchcity, searchstate)
+
 	doc = BeautifulSoup(rawhtml, "html.parser")
 
 	#strip away anything besides results
-	searchresults = doc.find('div', class_='post-content').find_all('ul') #FOR SELENIUM TODO REMOVE LIMIT
-	#searchresults = doc.find('div', class_='post-content').find_all('ul') #FOR DIRECT POST REQUESTS #NOT ABLE TO CAPTURE RESULTS IN RESPONSE - NEED BETTER HEADERS?
+	searchresults = doc.find('div', class_='post-content').find_all('ul')
 	
 	mayorcontactlist = []
 
@@ -99,8 +74,11 @@ def parse_mayors_data (searchcity, searchstate):
 
 def main():
     #MASTER VARIABLES
-	searchcity = "Seattle"
-	searchstate = "WA"
+	searchcity = "New York"
+	searchstate = "NY"
+
+	searchcity = sys.argv[1]
+	searchstate = sys.argv[2]
 
 	mayor = parse_mayors_data(searchcity,searchstate)
 	print(mayor)
